97e32f49a7dd0c635dabed65cbcc558a852e58b1
anghenfil <keanu.doelle@gmx.de>
2017-11-30 17:31:08 +0100

    Now also save site names

--- config.php
+++ config.php
@@ -3,3 +3,6 @@
 //DEBUG -> all errors
 //NONE -> no errors
 define("ERROR_LEVEL", "DEBUG");
+
+define("SITE_LIST", array("header", "footer", "dashboard", "upload"));
+define("SITE_NAMES", array("header"=>"", "footer"=>"", "dashboard"=>"Dashboard", "upload"=>"Upload"));
