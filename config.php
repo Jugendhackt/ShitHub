670d464b2422bcb205702004e8d32e68ce07305e
anghenfil <keanu.doelle@gmx.de>
2017-12-01 20:55:00 +0100

    Added test database configuration

--- config.php
+++ config.php
@@ -6,3 +6,10 @@ define("ERROR_LEVEL", "DEBUG");
 
 define("SITE_LIST", array("header", "footer", "dashboard", "upload"));
 define("SITE_NAMES", array("header"=>"", "footer"=>"", "dashboard"=>"Dashboard", "upload"=>"Upload"));
+
+define("UPLOAD_DIR", "data/snippets");
+
+define("DB_HOST", "localhost")
+define("DB_NAME", "shithub");
+define("DB_UNAME", "shithub");
+define("DB_PW", "test");
