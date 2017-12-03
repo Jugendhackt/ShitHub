a369c253302e5c367d3fa3018e7af40addb652c5
anghenfil <keanu.doelle@gmx.de>
2017-12-03 18:39:03 +0100

    Completed two methods

--- ShitHub/SQL/ShitHubSQL.php
+++ ShitHub/SQL/ShitHubSQL.php
@@ -15,9 +15,29 @@ class ShitHubSQL{
 	public function save_snippet($title, $description, $language, $tags){
 		if($this->pdo != null){
 			$query = $this->pdo->prepare("INSERT INTO snippets (title, description, language, tags) VALUES (?, ?, ?, ?);");
-			$query->execute(array($title, $description, $language, $tags));
+			if($query->execute(array($title, $description, $language, $tags))){
+				return $this->pdo->lastInsertId();
+			}else{
+				\ShitHub\Core\Loader::getLogger()->alert('SQL Error: '.$query->queryString.': '.$query->errorInfo()[2]);
+			}
+		}//TODO: Handle if pdo is null
+	}
+
+	public function load_snippet($id){
+		if($this->pdo != null){
+			$query = $this->pdo->prepare("SELECT title, description, language, tags FROM snippets WHERE id = ?");
+			if($query->execute(array($id))){
+				if($query->rowCount() > 0){
+					$row = $query->fetch();
+					return array($row['title'], $row['description'], $row['language'], $row['tags']);
+				}else{
+					return null;
+				}
+			}else{
+				\ShitHub\Core\Loader::getLogger()->alert('SQL Error: '.$query->queryString.': '.$query->errorInfo()[2]);
+			}
 
 			return $this->pdo->lastInsertId();
-		}
+		}//TODO: Handle if pdo is null
 	}
 }
