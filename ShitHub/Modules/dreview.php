c1d14a39caf3d71d4c6c677c4cbd7daf85321180
anghenfil <keanu.doelle@gmx.de>
2017-12-03 18:40:52 +0100

    Added site for showing code and review it

---
+++ ShitHub/Modules/dreview.php
@@ -0,0 +1,21 @@
+<?php
+namespace ShitHub\Modules;
+
+class dreview{
+	private $id;
+	private $code;
+
+	public function call_modul(...$args){
+		$this->id = intval($_GET['id']);
+
+		$sql = new \ShitHub\SQL\ShitHubSQL();
+
+		$this->code = $sql->load_snippet($this->id);
+
+		if($this->code != null && file_exists ('data/snippets/'.$this->id.'.snippet')){
+			\ShitHub\Templater\TemplateParser::set_variable("displaycode", file_get_contents('data/snippets/'.$this->id.'.snippet'));
+		}else{
+			header('Location: ?site=404');
+		}
+	}
+}
