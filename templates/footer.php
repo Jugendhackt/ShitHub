372fc4145ddf895625516ff5308eee21715a92da
anghenfil <keanu.doelle@gmx.de>
2017-12-09 16:25:14 +0100

    Corrected paths

--- templates/footer.php
+++ templates/footer.php
@@ -2,9 +2,9 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
 
-    <script src="/static/jquery-3.1.1.slim.min.js"></script>
+    <script src="libs/bootstrap/jquery-3.1.1.slim.min.js"></script>
     <script>window.jQuery || document.write('<script src="/ShitHub/static/jquery-slim.min.js"><\/script>')</script>
-    <script src="/static/popper.min.js"></script>
-    <script src="/static/bootstrap.min.js"></script>
+    <script src="libs/bootstrap/popper.min.js"></script>
+    <script src="libs/bootstrap/bootstrap.min.js"></script>
   </body>
 </html>
