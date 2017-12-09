84d6fb764330a2b400a5ed6126ec4ed2291ee621
anghenfil <keanu.doelle@gmx.de>
2017-12-09 17:40:27 +0100

    Revert "Revert "Added tabs""
    This reverts commit 5dfd04ea101f58543b84ebd0d73a5e3ce76f7b3a.

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -1,55 +1,15 @@
 <main role="main" class="container">
-
-      <div class= "container">
-        <div class= "row">
-            <div class= "col-sm" align= "center">
-                <h3> Austehende Reviews </h3>
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th scope= "col"></th>
-                            <th scope= "col"> Datum </th>
-                            <th scope= "col"> Review </th>
-                            <th scope= "col"> User </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                                <td>25.11.2017</td>
-                                <td>Jugend hackt Projekt</td>
-                                <td>anghofus</td>
-                            </th>
-                        </tr>
-                    </tbody> 
-                </table>
-    
-            </div>
-            
-            <div class= "col-sm" align= "center">
-                <h3> Letzte Review </h3>
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th scope= "col"></th>
-                            <th scope= "col"> Datum </th>
-                            <th scope= "col"> Review </th>
-                            <th scope= "col"> User </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                                <td>25.11.2017</td>
-                                <td>Jugend hackt Projekt</td>
-                                <td>anghofus</td>
-                            </th>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-                
-      </div>
-
-    </main><!-- /.container -->
+    <div class="right-tabs clearfix">
+        <ul class="nav nav-tabs">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Neuste</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Umstrittene</a>
+          </li>
+        </ul>
+    <div class="tab-content">
+        Test
+    </div>
+    </div>
+</main><!-- /.container -->
