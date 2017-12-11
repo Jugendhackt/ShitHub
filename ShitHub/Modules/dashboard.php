f69e30f05a71f26a29772813a2f74af4c59a64d4
anghenfil <keanu.doelle@gmx.de>
2017-12-11 12:24:57 +0100

    Now display data from database

--- ShitHub/Modules/dashboard.php
+++ ShitHub/Modules/dashboard.php
@@ -4,6 +4,27 @@ namespace ShitHub\Modules;
 
 class dashboard{
 	public function call_modul(...$args){
+		$sql = new \ShitHub\SQL\ShitHubSQL();
+		$parser = new \ShitHub\Templater\TemplateParser("templates/dashboard_row.php");
+		$result = $sql->get_unresponded_reviews(5);
+		
+		$full = "";		
+		foreach($result as $key){
+			$tags = "";
+			$temp = explode(", ", $key['tags']);
+			$tagparser = new \ShitHub\Templater\TemplateParser("templates/dashboard_row_tags_tag.php");
 
+			foreach($temp as $tag){
+				\ShitHub\Templater\TemplateParser::set_variable("dashboard_row_tags_tag", $tag);
+				$tags .= $tagparser->parseReturn();
+			}
+
+			\ShitHub\Templater\TemplateParser::set_variable("dashboard_row_title", $key['title']);
+			\ShitHub\Templater\TemplateParser::set_variable("dashboard_row_author", $key['author_name']);
+			\ShitHub\Templater\TemplateParser::set_variable("dashboard_row_tags", $tags);
+			$full .= $parser->parseReturn();
+		}
+
+		\ShitHub\Templater\TemplateParser::set_variable("dashboard_tab_content", $full);
 	}
 }
--- ShitHub/SQL/ShitHubSQL.php
+++ ShitHub/SQL/ShitHubSQL.php
@@ -26,18 +26,39 @@ class ShitHubSQL{
 	public function load_snippet($id){
 		if($this->pdo != null){
 			$query = $this->pdo->prepare("SELECT title, description, language, tags FROM snippets WHERE id = ?");
-			if($query->execute(array($id))){
-				if($query->rowCount() > 0){
-					$row = $query->fetch();
-					return array($row['title'], $row['description'], $row['language'], $row['tags']);
-				}else{
-					return null;
-				}
-			}else{
-				\ShitHub\Core\Loader::getLogger()->alert('SQL Error: '.$query->queryString.': '.$query->errorInfo()[2]);
+			$query->execute(array($id));
+			$row = $query->fetch();
+			
+			return $row;
+		}//TODO: Handle if pdo is null
+	}
+
+	public function get_unresponded_reviews(int $anz){
+		if($this->pdo != null){
+			$query = $this->pdo->prepare("SELECT author_id, author_name, title, language, tags FROM snippets LIMIT :limit"); //TODO: implement state 
+			$query->bindParam(':limit', $anz, \PDO::PARAM_INT);
+			$query->execute();
+
+			$array = array();
+			while($row = $query->fetch()){
+				array_push($array, $row);
+			}
+			return $array;
+		}//TODO: Handle if pdo is null
+	}
+
+	public function get_last_Review(int $anz){
+		if($this->pdo != null){
+			$query = $this->pdo->prepare("SELECT  author_id, author_name, title, language, tags FROM snippets LIMIT :limit"); //TODO: implement state 
+			$query->bindParam(':limit', $anz, \PDO::PARAM_INT);
+			$query->execute();
+
+			$array = array();
+			while($row = $query->fetch()){
+				array_push($row, $array);
 			}
 
-			return $this->pdo->lastInsertId();
+			return $array;
 		}//TODO: Handle if pdo is null
 	}
 }
