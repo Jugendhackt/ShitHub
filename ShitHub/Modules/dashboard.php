4584aabb75a0e95487a460b6eb25f4373ea9c903
anghenfil <keanu.doelle@gmx.de>
2017-12-21 19:55:39 +0100

    Tabs now working

--- ShitHub/Modules/dashboard.php
+++ ShitHub/Modules/dashboard.php
@@ -4,9 +4,26 @@ namespace ShitHub\Modules;
 
 class dashboard{
 	public function call_modul(...$args){
+		if(isset($_GET['tab'])){
+			$tab = $_GET['tab'];
+		}else{
+			$tab = "newest";
+		}
+
 		$sql = new \ShitHub\SQL\ShitHubSQL();
 		$parser = new \ShitHub\Templater\TemplateParser("templates/dashboard_row.php");
-		$result = $sql->get_unresponded_reviews(5);
+
+		\ShitHub\Templater\TemplateParser::set_variable("newest_active", "");
+		\ShitHub\Templater\TemplateParser::set_variable("discussed_active", "");
+
+		if($tab == "discussed"){
+			\ShitHub\Templater\TemplateParser::set_variable("discussed_active", "active");
+			$result = $sql->get_discussed_reviews(5);
+		}else{
+			\ShitHub\Templater\TemplateParser::set_variable("newest_active", "active");
+			$result = $sql->get_newest_reviews(5);
+		}
+		
 		
 		$full = "";		
 		foreach($result as $key){
@@ -20,7 +37,9 @@ class dashboard{
 			}
 
 			\ShitHub\Templater\TemplateParser::set_variable("dashboard_row_title", $key['title']);
-			\ShitHub\Templater\TemplateParser::set_variable("dashboard_row_author", $key['author_name']);
+			\ShitHub\Templater\TemplateParser::set_variable("dashboard_row_author_name", $key['author_name']);
+			\ShitHub\Templater\TemplateParser::set_variable("dashboard_row_author_id", $key['author_id']);
+			\ShitHub\Templater\TemplateParser::set_variable("dashboard_row_date", $key['date']);
 			\ShitHub\Templater\TemplateParser::set_variable("dashboard_row_tags", $tags);
 			$full .= $parser->parseReturn();
 		}
