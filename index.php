5e6f728c7febbd9113dca9ee6bc57ba184609a9d
anghenfil <keanu.doelle@gmx.de>
2017-12-24 17:38:21 +0100

    bugfix

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 define("SECURITY", "TRUE");	
 
+require_once __DIR__ . '/vendor/autoload.php'; //Start autoloader
+$dotenv = new Dotenv\Dotenv('/var/www/', 'config.env'); //Path for config comes here
+$dotenv->load();
+
 if($_ENV['ERROR_LEVEL'] == "DEBUG"){
 	ini_set("display_errors", 1);
 }else{
 	ini_set("display_errors", 0);
 }
-
-require_once __DIR__ . '/vendor/autoload.php'; //Start autoloader
-$dotenv = new Dotenv\Dotenv('/var/www/', 'config.env'); //Path for config comes here
-$dotenv->load();
 
 $loader = new \ShitHub\Core\Loader();
 $loader->load();
