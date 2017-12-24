1d3a37dc1bd3b618c2bec5c4ae13e5b6ff81708d
anghenfil <keanu.doelle@gmx.de>
2017-12-24 17:37:34 +0100

    Show errors if in debug mode on site

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 define("SECURITY", "TRUE");	
 
-ini_set("display_errors", 0);
+if($_ENV['ERROR_LEVEL'] == "DEBUG"){
+	ini_set("display_errors", 1);
+}else{
+	ini_set("display_errors", 0);
+}
 
 require_once __DIR__ . '/vendor/autoload.php'; //Start autoloader
 $dotenv = new Dotenv\Dotenv('/var/www/', 'config.env'); //Path for config comes here
