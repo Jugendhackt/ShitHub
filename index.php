4ec85b0f146e16be956ebcb3c86e8ae12a7202f5
anghenfil <keanu.doelle@gmx.de>
2017-12-24 17:35:20 +0100

    Adjusted error reporting system.

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 define("SECURITY", "TRUE");	
+
+ini_set("display_errors", 0);
+
 require_once __DIR__ . '/vendor/autoload.php'; //Start autoloader
 $dotenv = new Dotenv\Dotenv('/var/www/', 'config.env'); //Path for config comes here
 $dotenv->load();
-
-if($_ENV['ERROR_LEVEL'] == "DEBUG"){ //Read out error level, set error reporting settings
-	error_reporting(E_ALL);
-	ini_set("display_errors", 1);
-}
 
 $loader = new \ShitHub\Core\Loader();
 $loader->load();
