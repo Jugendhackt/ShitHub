3b588e1c06187a6dfa6dcf549b05e4d512101f0b
anghenfil <keanu.doelle@gmx.de>
2017-12-25 23:53:07 +0100

    Added user settings

--- templates/profil_menu.php
+++ templates/profil_menu.php
@@ -5,7 +5,10 @@ die("Direct invocation isn't allowed.");
         <a class="nav-link dropdown-toggle" href="#" id="dropdown-review" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
              Profil
         </a>
-    <div class="dropdown-menu" aria-labelledby="dropdown-review">
+    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown-review">
+        <a class="dropdown-item" href="?site=u_controlcenter">Control Center</a>
+        <a class="dropdown-item" href="?site=u_settings">Profileinstellungen</a>
+        <hr>
         <a class="dropdown-item" href="?site=logout">Logout</a>
     </div>
 </li>
