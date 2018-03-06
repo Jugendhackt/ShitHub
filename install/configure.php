b8bf6b92865372e9e6f14d0a299b7aabdbab5616
anghenfil <keanu.doelle@gmx.de>
2018-03-06 10:44:25 +0100

    Now only use path without filename

--- install/configure.php
+++ install/configure.php
@@ -24,7 +24,7 @@ $form = "<form action=\"#\" method=\"post\">
     <input type='text' size='75' name=\"uploaddir\" value=".dirname(__DIR__)."/data/upload>
     <p>Log location</p>
     <input type='text' size='75' name=\"loglocation\" value=".dirname(__DIR__)."/data/log>
-    <p>Config location with filename (IMPORTANT: OUT OF WEBROOT!)</p>
+    <p>Config location without filename (IMPORTANT: OUT OF WEBROOT!)</p>
     <input type='text' size='75' name=\"configlocation\">
     <button type='submit' name='configureform'>Proceed</button>
 </form>";
@@ -35,7 +35,7 @@ if(!isset($_POST['configureform'])){
     if(empty($_POST['loglocation']) || empty($_POST['uploaddir']) || empty($_POST['debuglevel']) || empty($_POST['loglocation']) || empty('configlocation')){
         print($form);
     }else{
-        if(!copy(__DIR__."/default_config", $_POST['configlocation'])){
+        if(!copy(__DIR__."/default_config", $_POST['configlocation'].'config.env')){
             print("<b style='color: red'>Couldn't create config. Please check permissions.</b>");
             print($form);
         }else{
@@ -48,8 +48,9 @@ if(!isset($_POST['configureform'])){
 
             $writedata .= "\nUPLOAD_DIR = ".$_POST['uploaddir'];
             $writedata .= "\nLOG_LOCATION = ".$_POST['loglocation'];
-            //TODO: Write rest of config
-            header("Location: ?step3&config=".$_POST['configlocation']);
+
+            $writer = file_put_contents($_POST['configlocation'].'config.env', $writedata, FILE_APPEND | LOCK_EX);
+            header("Location: ?step3&config=".$_POST['configlocation'].'config.env');
         }
     }
 }
