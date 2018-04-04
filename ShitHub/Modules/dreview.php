2d26669b634b2f81efd76bb32e483debcca21088
anghenfil <keanu.doelle@gmx.de>
2018-04-05 00:20:24 +0200

    Changed and corrected syntax highlighting; Now use snippet language for highlighting.

--- ShitHub/Modules/dreview.php
+++ ShitHub/Modules/dreview.php
@@ -10,34 +10,33 @@ if(!defined(SECURITY)){
 
 class dreview{
 	private $id;
-	private $code;
+	private $snippet;
 
 	public function call_modul(...$args){
 	    TemplateParser::$globalstore->push("customcss", "<link href=\"vendor/scrivo/highlight.php/styles/default.css\" rel=\"stylesheet\"");
 		$this->id = intval($_GET['id']);
 
 		$sql = new ShitHubSQL();
-		$this->code = $sql->load_snippet($this->id);
+		$this->snippet = $sql->load_snippet($this->id);
 
-		if($this->code != null && file_exists ('data/snippets/'.$this->id.'.snippet')){
+		if($this->snippet != null && file_exists ('data/snippets/'.$this->id.'.snippet')){
 			$highlighter = new \Highlight\Highlighter();
             try {
-                $code = $highlighter->highlight('cpp', html_entity_decode(file_get_contents('data/snippets/' . $this->id . '.snippet')));
+                $code = $highlighter->highlight($this->snippet['language'], html_entity_decode(file_get_contents('data/snippets/' . $this->id . '.snippet')));
+                $codearray = explode("\n", $code->value);
+                $size = sizeof($codearray);
+                unset($codearray);
+
+                $nums = "";
+                for($i = 0;$i<$size;$i++){
+                    $nums .= $i."\n";
+                }
+
+                TemplateParser::$globalstore->set_variable("displaycode", $code->value);
+                TemplateParser::$globalstore->set_variable("displaynums", $nums);
             } catch (\Exception $e) {
-                //TODO: Handle error
+                //TODO: Error handling
             }
-            $codearray = explode("\n", $code->value);
-			$size = sizeof($codearray);
-			unset($codearray);
-
-			$nums = "";
-			$i = 1;
-			for($i = 0;$i<$size;$i++){
-				$nums .= $i."\n";
-			}
-
-			TemplateParser::$globalstore->set_variable("displaycode", $code->value);
-			TemplateParser::$globalstore->set_variable("displaynums", $nums);
 		}else{
 			header('Location: ?site=404');
 		}
