a15862fede52cf19aba81d2cfea5b991fe9241ee
anghenfil <keanu.doelle@gmx.de>
2018-05-12 15:08:38 +0200

    Added class SnippetView

---
+++ ShitHub/CodeViewer/SnippetView.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anghenfil
+ * Date: 12.05.18
+ * Time: 14:39
+ */
+
+namespace ShitHub\CodeViewer;
+
+
+class SnippetView{
+    private $snippetID;
+
+    public function __construct(){
+
+    }
+
+    public function generateSnippetView(){
+
+    }
+
+    private function highlightCode(){
+
+    }
+}
