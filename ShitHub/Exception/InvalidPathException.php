d597b00d2fe6fd6f0ef86cd8fd0dcc7cf11c33a1
anghenfil <keanu.doelle@gmx.de>
2018-05-12 15:38:00 +0200

    Created InvalidPathException

---
+++ ShitHub/Exception/InvalidPathException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anghenfil
+ * Date: 12.05.18
+ * Time: 15:35
+ */
+
+namespace ShitHub\Exception;
+
+use InvalidArgumentException;
+
+class InvalidPathException extends InvalidArgumentException {
+
+}
