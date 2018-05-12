c5ca88c057aaa03a7c275a6a5b777599e428bee5
anghenfil <keanu.doelle@gmx.de>
2018-05-12 15:05:23 +0200

    Moved project and snippet check to static methods to use in other classes

--- ShitHub/CodeViewer/CodeViewer.php
+++ ShitHub/CodeViewer/CodeViewer.php
@@ -12,7 +12,7 @@ use ShitHub\SQL\CodeViewerSQL;
 
 class CodeViewer{
     private $projectID;
-    private $activeSnippet;
+    private $activeSnippet = null;
     private $mode;
     private $navigation;
 
@@ -23,35 +23,28 @@ class CodeViewer{
      * @param boolean false $editmode view or edit mode; false = read mode true = write mode
      */
     public function __construct($project, $snippet = null, $editmode = false){
-        $db = new CodeViewerSQL();
+        if (self::checkProject($project)) {
+            $this->projectID = $project;
+            $this->navigation = new Navigation($project);
 
-        if(!is_null($project) && is_int($project)){ //CheckprojectID
-            if($db->checkProjectID($project)) {
-                $this->projectID = $project; //Assign projectid
-
-                if (!is_null($snippet)) {
-                    if (is_int($snippet)) { //Check if snippetID is int
-                        if($db->checkSnippetID($snippet, $project)) { //Check if snippetID is in project
-                            $this->activeSnippet = $snippet;
-                        }else{
-                            throw new \InvalidArgumentException("snippet doesn't belong to project");
-                        }
+            if (!is_null($snippet)) {
+                if (self::checkSnippet($snippet)) {
+                    if (self::checkSnippetInProject($snippet, $project)) {
+                        $this->activeSnippet = $snippet;
                     } else {
-                        throw new \InvalidArgumentException("snippetID as second parameter must be int or unset");
+                        throw new \InvalidArgumentException("snippet doesn't belong to project");
                     }
-                }
-                if (is_bool($editmode)) { //Check if editmode is bool
-                    $this->mode = $editmode;
                 } else {
-                    throw new \InvalidArgumentException("mode as second parameter must be boolean: false (read) or true (read/write)");
+                    throw new \InvalidArgumentException("snippetID as second parameter must be int or null");
                 }
-
-                $this->navigation = new Navigation($project); //Create navigation object
-            }else{
-                throw new \InvalidArgumentException("projectID not valid");
             }
-        }else{
-            throw new \InvalidArgumentException("projectID as first parameter (int) required");
+            if (is_bool($editmode)) { //Check if editmode is bool
+                $this->mode = $editmode;
+            } else {
+                throw new \InvalidArgumentException("mode as third parameter must be boolean: false (read) or true (read/write)");
+            }
+        } else {
+            throw new \InvalidArgumentException("Valid projectID as first parameter (int) required");
         }
     }
 
@@ -59,6 +52,42 @@ class CodeViewer{
      * Shows CodeViewer
      */
     public function show(){
+        $this->navigation->generateNavigation();
+    }
 
+    static public function checkSnippet($snippet){
+        if (!is_null($snippet)) {
+            if (is_int($snippet)) { //Check if snippetID is int
+                return true;
+            } else {
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    static public function checkSnippetInProject($snippet, $project){
+        $db = new CodeViewerSQL();
+
+        if($db->checkSnippetID($snippet, $project)) { //Check if snippetID is in project
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    static public function checkProject($project){
+        $db = new CodeViewerSQL();
+
+        if(!is_null($project) && is_int($project)) { //CheckprojectID
+            if ($db->checkProjectID($project)) {
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
     }
 }
