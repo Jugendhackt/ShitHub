8fdb646c32bef089a2f9f1bf240540366df9337d
anghenfil <keanu.doelle@gmx.de>
2018-06-29 14:35:47 +0200

    extended SnippetView

--- ShitHub/src/CodeViewer/SnippetView.php
+++ ShitHub/src/CodeViewer/SnippetView.php
@@ -9,13 +9,28 @@
 namespace ShitHub\CodeViewer;
 
 
-use anghenfil\Templater\TemplateParser;
+use anghenfil\Templater\VariableStore;
 
 class SnippetView{
 
     public function generateSnippetView($snippet){
         if($snippet instanceof Snippet){ //parameter snippet must be object type Snippet
-            $this->highlightCode($snippet);
+            $code = "";
+            $highlighted = $this->highlightCode($snippet);
+
+            if(!is_null($highlighted)){
+                $code = $highlighted;
+            }else{ //Fallback if highlight failed
+                $code = html_entity_decode(file_get_contents($snippet->getFilepath()));
+            }
+
+            $rownums = $this->generateLineNumbers($code);
+
+            $vs = new VariableStore();
+            $vs->set_variable("code", $code);
+            $vs->set_variable("rownums", $rownums);
+
+            return $vs;
         }else{
             throw new \InvalidArgumentException("Parameter must be type Snippet");
         }
@@ -25,19 +40,22 @@ class SnippetView{
         $highlighter = new \Highlight\Highlighter();
         try {
             $code = $highlighter->highlight($snippet->getLanguage(), html_entity_decode(file_get_contents($snippet->getFilepath())));
-            $codearray = explode("\n", $code->value);
-            $size = sizeof($codearray);
-            unset($codearray);
-
-            $nums = "";
-            for($i = 0;$i<$size;$i++){
-                $nums .= $i."\n";
-            }
-
-            TemplateParser::$globalstore->set_variable("displaycode", $code->value);
-            TemplateParser::$globalstore->set_variable("displaynums", $nums);
+            return $code->value;
         } catch (\Exception $e) {
-            //TODO: Error handling
+            return null; //If Highlighting fails, return false to turn it off
         }
+    }
+
+    private function generateLineNumbers($code){
+        $codearray = explode("\n", $code->value);
+        $size = sizeof($codearray);
+        unset($codearray);
+
+        $nums = "";
+        for($i = 0;$i<$size;$i++){
+            $nums .= $i."\n";
+        }
+
+        return $nums;
     }
 }
